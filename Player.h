9f529ad48afa62be5c380600703bb5e0673e10bc
Brian Lewis <blink182515@gmail.com>
2012-04-16 17:27:33 -0600

    Added Objective C class Player File, for creating Player class

---
+++ Player.h
@@ -0,0 +1,13 @@
+//
+//  Player.h
+//  Blind Man's Bluff
+//
+//  Created by Brian Lewis on 4/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Player : NSObject
+
+@end
